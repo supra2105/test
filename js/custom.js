53bfd1c718d45d74de9c4cc11bbf911e22775796
Surya <sbabbar_bemba16@thapar.edu>
2021-04-18 16:57:28 +0530

    Changed the slide handlers and added php form handler.

--- js/custom.js
+++ js/custom.js
@@ -154,14 +154,7 @@
     if ($(".fullpage-default").length) {
       var myFullpage = new fullpage(".fullpage-default", {
         licenseKey: " C7F41B00-5E824594-9A5EFB99-B556A3D5",
-        anchors: [
-          "slide01",
-          "slide02",
-          "slide03",
-          // "slide05",
-          "slide06",
-          "slide07",
-        ],
+        anchors: ["top", "about", "skills", "work", "contact"],
         menu: "#nav",
         lazyLoad: true,
         navigation: true,
